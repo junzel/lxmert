ba1fa04b991223fd73fe9f9c1fc6e2178281241c
Junze Liu <liu.junze@outlook.com>
2020-03-22 21:52:10 -0700

    update

--- sceneGraph_scripts/pairwise_relations.py
+++ sceneGraph_scripts/pairwise_relations.py
@@ -12,6 +12,35 @@ TRAIN_SCENE_GRAPHS = 'data/gqa/normalized_train_sceneGraphs.json'
 VAL_SCENE_GRAPHS = 'data/gqa/normalized_valid_sceneGraphs.json'
 GRAPH_MAPPING = 'data/gqa/graph_mapping.json'
 
+def bb_iou(boxA, boxB):
+    """Calculate the Intersection of Union of boxes from Faster RCNN and scene graph"""
+    # determine the (x, y)-coordinates of the intersection rectangle
+    xA = max(boxA[0], boxB[0])
+    yA = max(boxA[1], boxB[1])
+    xB = min(boxA[2], boxB[2])
+    yB = min(boxA[3], boxB[3])
+
+    # compute the area of intersection rectangle
+    interArea = abs(max((xB - xA, 0)) * max((yB - yA), 0))
+    if interArea == 0:
+        return 0
+    # compute the area of both the prediction and ground-truth
+    # rectangles
+    boxAArea = abs((boxA[2] - boxA[0]) * (boxA[3] - boxA[1]))
+    boxBArea = abs((boxB[2] - boxB[0]) * (boxB[3] - boxB[1]))
+
+    # compute the intersection over union by taking the intersection
+    # area and dividing it by the sum of prediction + ground-truth
+    # areas - the interesection area
+    iou = interArea / float(boxAArea + boxBArea - interArea)
+
+    # return the intersection over union value
+    return iou
+
+
+
+
+
 def relation_tensor(object_ids, scene_graph_objects, relation_mapping):
     """
     object_ids (list of dict): each bbox's matching object id
